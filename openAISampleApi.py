8f3ab2faa61937de8ead17e9fce43d48f6607f34
Sergio Negri <sergio.negri@gmail.com>
2022-12-08 10:40:45 +0400

    Adding sample OpenAI API call

---
+++ openAISampleApi.py
@@ -0,0 +1,21 @@
+import os
+import openai
+import json
+
+# TODO load API key from an environment variable or secret management service
+#openai.api_key = os.getenv("OPENAI_API_KEY")
+
+openai.api_key = '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'
+
+input='List 10 movies that are similar to interstellar, memento and tenet'
+
+response = openai.Completion.create(model="text-davinci-003", 
+                                    prompt=input, 
+                                    temperature=0.5,
+                                    max_tokens=250,
+                                    top_p=1.0,
+                                    frequency_penalty=0.0,
+                                    presence_penalty=0.0
+                                   )
+
+print(response["choices"][0]["text"])
